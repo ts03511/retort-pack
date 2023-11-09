5659897401a1491968dc6692317c04d0ec9f554b
ts03511 <ts03511@gmail.com>
2023-11-09 10:08:01 +0900

    ディレクトリ整理とindex.html少し書き換え

--- finance/html/common/get_history.php
+++ finance/html/common/get_history.php
@@ -3,10 +3,10 @@
 		while ($history = $sql_result->fetch_assoc()){
 			if ($history['type']) {
 				print "<tr>\n";
-				    print "<td><span class=\"cNo\">{$menu_info['id']}</span></td>\n";
-				    print "<td><span class=\"cBrewery\">{$menu_info['registered_date']}</span><br>\n";
-				    print "<td><span class=\"cBeername\">{$menu_info['type']}</span></span><br>\n";
-				    print "<td><span class=\"cLocality\">{$menu_info['value']}</span></span></td>\n";
+				    print "<td><span class=\"cId\">{$menu_info['id']}</span></td>\n";
+				    print "<td><span class=\"cDate\">{$menu_info['registered_date']}</span><br>\n";
+				    print "<td><span class=\"cType\">{$menu_info['type']}</span></span><br>\n";
+				    print "<td><span class=\"cValue\">{$menu_info['value']}</span></span></td>\n";
 				print "</tr>\n\n";
 			};
 		};
--- tools/initialize_retort-pack.sh
+++ tools/initialize_retort-pack.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+## middleware install
+sudo dnf -y install httpd php php-mysqlnd
+
+## directory initializing and deploy
 sudo rm -rf /var/www/retort-pack
 sudo rm -rf /var/www/credential.php
 sudo mkdir -p /var/www/retort-pack
-sudo rsync -av . /var/www/retort-pack > /dev/null
+sudo rsync -av .. /var/www/retort-pack > /dev/null
 sudo cp credential.php.sample /var/www/credential.php
 sudo chown -R apache:apache /var/www/retort-pack
 sudo chown apache:apache /var/www/credential.php
+
+## processing complete
 echo retort-pack Initialize complete.
 echo please create Databse and edit /var/www/retort-pack/credential.php
