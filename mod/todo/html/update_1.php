2ad43dd7281b3bb571ef01078918c6adc79b4c5a
ts03511 <ts03511@gmail.com>
2023-11-15 21:59:04 +0900

    create base of input display.

--- mod/todo/html/update_1.php
+++ mod/todo/html/update_1.php
@@ -6,60 +6,21 @@
 	
 </head>
 
-<!-- List of valiables.
-	$db_connect -> Database connect instance.
-	$db_host -> Database Host IP Address.
-	$db_user -> Database User.
-	$db_password -> Database User's Password.
-	$db_name -> Database's Name.
-	$menu_sql -> SQL.
-	$sql_result -> exec SQL Result.
-	$menu_info -> Selected menu information.
--->
-
 	<body>
-			<?php
-				require './common/connect_to_db.php';
-			?>
 
 		<div id="master">
 			<form action="./update_2.php" method="POST">
-				<table>
-					<tr>
-						<th>NO</th>
-						<th>BREWERY</th>
-						<th>BEERNAME</th>
-						<th>Locality</th>
-						<th>Style</th>
-						<th>ABV</th>
-					</tr>
-
-					<?php
-						$menu_sql = 'SELECT * FROM beer_menu;';
-						if ($sql_result = $db_connect->query($menu_sql)) {
-							while ($menu_info = $sql_result->fetch_assoc()){
-								print "<tr>\n";
-								print "<td>{$menu_info['no']}</td>\n";
-								print "<td><input type=\"text\" name=\"no{$menu_info['no']}_brewery1\" value=\"{$menu_info['brewery1']}\" class=\"fValue\"><br>\n";
-								print "<input type=\"text\" name=\"no{$menu_info['no']}_brewery2\" value=\"{$menu_info['brewery2']}\" class=\"fValue\"></td>\n";
-								print "<td><input type=\"text\" name=\"no{$menu_info['no']}_beername1\" value=\"{$menu_info['beername1']}\" class=\"fValue\"><br>\n";
-								print "<input type=\"text\" name=\"no{$menu_info['no']}_beername2\" value=\"{$menu_info['beername2']}\" class=\"fValue\"></td>\n";
-								print "<td><input type=\"text\" name=\"no{$menu_info['no']}_locality\" value=\"{$menu_info['locality']}\" class=\"fValue\"></td>\n";
-								print "<td><input type=\"text\" name=\"no{$menu_info['no']}_style1\" value=\"{$menu_info['style1']}\" class=\"fValue\"><br>\n";
-								print "<input type=\"text\" name=\"no{$menu_info['no']}_style2\" value=\"{$menu_info['style2']}\" class=\"fValue\"></td>\n";
-								print "<td><input type=\"text\" name=\"no{$menu_info['no']}_abv\" value=\"{$menu_info['abv']}\" class=\"fValue\"></td>\n";
-								print "</tr>\n\n";
-							};
-						};
-						$sql_result->close();
-						$db_connect->close();
-					?>
-				</table>
-				<input type="submit" value="この内容で更新する">
+				<div id="title_area">件名： <input type="text" id="title"></div><br><br>
+				<div id="title_area">： 概要<br>
+				<textarea name="example" cols="50" rows="100"></textarea>
+				<div id="title_area">状態： <input type="text" id="title"></div>
+				<div id="title_area">開始日： <input type="text" id="title"></div>
+				<div id="title_area">期限日： <input type="text" id="title"></div>
+				<input type="submit" value="プレビュー">
 			</form>
 
 			<br><br>
-			<input type="button" value="ビールメニューへ" onClick="location.href='./menu.php'">
+			<input type="button" value="戻る" onClick="location.href='./menu.php'">
 		</div>
 	</body>
 </html>
